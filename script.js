ecb3fca484bcc0e43f9a4405d260c2472d80b66b
Chilik78 <dmitry.cheliadinov@yanex.ru>
2022-06-23 11:56:48 +0300

    Без комментов

--- script.js
+++ script.js
@@ -132,9 +132,9 @@ myAudio2.onpause = function()
   isPlaying2 = false;
 };
 
-if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1		
-  var dpt = window.devicePixelRatio;
-  var widthM = window.screen.width * dpt;
-  var widthH = window.screen.height * dpt;
-  document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
-}
+// if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1		
+//   var dpt = window.devicePixelRatio;
+//   var widthM = window.screen.width * dpt;
+//   var widthH = window.screen.height * dpt;
+//   document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
+// }
